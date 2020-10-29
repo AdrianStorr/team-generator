8bbbb2ae89ebc53866abbc1d1cf4b79244ce9538
Adrian Storr <Adrianloves123@gmail.com>
2020-10-29 16:55:50 -0400

    added  files and made changes to protype

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const TeamProfileGenerator = require('./lib/TeamProfileGenerator');
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 
 const render = require("./lib/htmlRenderer");
 const team = new TeamProfileGenerator(questionList);
 team.launch();
+
 
 
 // Write code to use inquirer to gather information about the development team members,
